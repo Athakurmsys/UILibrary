1399f05890b11baeccaf9f133dfd4ac50847362e
Aman Thakur <athakur@cantaloupe.com>
2022-08-22 11:48:03 +0530

    Initial commit

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,9 +1,9 @@
 import React, { FC, ReactNode } from 'react';
 
 type Props = {
-   children: ReactNode;
-   onChange: (event: React.MouseEvent<HTMLButtonElement>) => void;
-   className: string;
+   children?: ReactNode;
+   onChange?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+   className?: string;
 }
 
 const Button:FC<Props> = ({children, onChange, className}) =>{
